568110ff739331017d870d8bf0e73d0af9dc2eca
Pim Ritmeijer <48485332+PRitmeijer@users.noreply.github.com>
2025-12-03 17:49:12 +0100

    update release flow

--- .github/workflows/management/checkrelease.py
+++ .github/workflows/management/checkrelease.py
@@ -5,19 +5,19 @@ from .utils import PATHS
 
 
 def main() -> None:
-    with open(os.environ["GITHUB_OUTPUT"], "w") as f:  # noqa: PTH123
+    with open(os.environ["GITHUB_OUTPUT"], "a") as f:  # noqa: PTH123
         if not PATHS.RELEASE_FILE.exists():
-            f.write("status=Release file doesn't exist")
+            f.write("status=no_release_file\n")
         else:
             try:
                 contents = PATHS.RELEASE_FILE.read_text("utf-8")
                 if not contents.strip():
-                    f.write("status=Release file is empty")
+                    f.write("status=empty_file\n")
                 else:
                     parse_release_file(contents)
-                    f.write(f"status={''}")
+                    f.write("status=ready\n")  # Ready to release
             except InvalidReleaseFileError:
-                f.write("status=Release file is invalid or missing release type")
+                f.write("status=invalid_file\n")
 
 
 if __name__ == "__main__":
